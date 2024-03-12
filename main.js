1ac8e6edd178e508ae5be9c826d8175c443e5c6a
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-12 12:32:55 +0500

    Add files via upload
    100 days of coding challenge day no 1 is complete

---
+++ main.js
@@ -0,0 +1,8 @@
+//  Q no 1
+var PersonName = "Safdar Ali";
+console.log("Hello ".concat(PersonName, " would you like to learn some typescript today?"));
+//  Q on 2 
+var Personname = "SafdarAli";
+console.log(Personname.toLowerCase());
+console.log(Personname.toUpperCase());
+console.log(Personname.toUpperCase().charAt(0) + Personname.toLowerCase().slice(1));
---
+++ main.ts
@@ -0,0 +1,14 @@
+// Q no 1 
+// VS Code and typescript insatalled
+
+//  Q no 2
+
+let PersonName : string ="Safdar Ali";
+console.log(`Hello ${PersonName} would you like to learn some typescript today?`);
+
+//  Q on 3
+
+let Personname :string ="SafdarAli";
+console.log(Personname.toLowerCase());
+console.log(Personname.toUpperCase());
+console.log(Personname.toUpperCase().charAt(0)+Personname.toLowerCase().slice(1));
